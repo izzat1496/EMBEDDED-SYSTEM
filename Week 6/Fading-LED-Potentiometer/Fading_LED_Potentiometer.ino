40c7c17c5bd55bc98a78a574cbe4a2a196b54f9d
izzat1496 <73817849+izzat1496@users.noreply.github.com>
2020-12-17 15:09:18 +0800

    Add files via upload

---
+++ Week 6/Fading-LED-Potentiometer/Fading_LED_Potentiometer.ino
@@ -0,0 +1,23 @@
+
+
+int LED_PIN = 6;  // the PWM pin the LED is attached to
+int POTENTIOMETER_PIN = A0; // setting the analog pin of the potentiometer
+
+void setup() {
+  
+  // declare LED pin to be an output:
+  pinMode(LED_PIN, OUTPUT);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  
+  // reads the input on analog pin A0 (value between 0 and 1023)
+  int analogValue = analogRead(POTENTIOMETER_PIN);
+
+  // scales it to brightness (value between 0 and 255)
+  int brightness = map(analogValue, 0, 1023, 0, 255);
+
+  // sets the brightness LED that connects to  pin 3
+  analogWrite(LED_PIN, brightness);
+}
